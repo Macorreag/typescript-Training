9869713ab7e0ac0b30be2e007a53df9800a16e10
macorreag <macorreag@unal.edu.co>
2019-03-18 15:37:33 -0500

    Tipos de parametros en Ts Normal,Defecto,Opcional

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo Typescript");
+function pruebaTestAngular(param1, param2, param3) {
+    /*  Param1 obligatorio
+        Param2 Tiene por defecto "Hola mundo"
+        Param3 es  opcional
+    */
+    if (param2 === void 0) { param2 = "Hola mundo"; }
+    console.log(param1);
+    console.log(param2);
+    console.log(param3);
+}
+pruebaTestAngular("Miller");
+pruebaTestAngular("Miller", "correa", "g");
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("Hola mundo Typescript");
+function pruebaTestAngular(
+    param1:string,
+    param2:string = "Hola mundo",
+    param3?:string){
+    /*  Param1 obligatorio
+        Param2 Tiene por defecto "Hola mundo"
+        Param3 es  opcional
+    */
+
+    console.log(param1);
+    console.log(param2);
+    console.log(param3);
+}
+pruebaTestAngular("Miller");
+pruebaTestAngular("Miller","correa","g");
