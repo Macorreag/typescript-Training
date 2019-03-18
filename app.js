2529fccac82295aab43f5fc5d0c390fde3785e9b
macorreag <macorreag@unal.edu.co>
2019-03-18 15:45:12 -0500

    Funciones flecha en Ts

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-function pruebaTestAngular(param1, param2, param3) {
-    /*  Param1 obligatorio
-        Param2 Tiene por defecto "Hola mundo"
-        Param3 es  opcional
-    */
-    if (param2 === void 0) { param2 = "Hola mundo"; }
-    console.log(param1);
-    console.log(param2);
-    console.log(param3);
-}
-pruebaTestAngular("Miller");
-pruebaTestAngular("Miller", "correa", "g");
+var add = function (x, y) { return x + y; };
+console.info(add(2, 4));
--- app.ts
+++ app.ts
@@ -1,15 +1,3 @@
-function pruebaTestAngular(
-    param1:string,
-    param2:string = "Hola mundo",
-    param3?:string){
-    /*  Param1 obligatorio
-        Param2 Tiene por defecto "Hola mundo"
-        Param3 es  opcional
-    */
+var add = (x: number , y: number) => x + y ;
 
-    console.log(param1);
-    console.log(param2);
-    console.log(param3);
-}
-pruebaTestAngular("Miller");
-pruebaTestAngular("Miller","correa","g");
+console.info( add(2,4));
